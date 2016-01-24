a729e972adc6f7b8f7e32a9fa1cc29c81d509b68
zhikiri <zhivitsa.kirill.gm@gmail.com>
2016-01-24 14:13:48 +0200

    Added the Middleware class

---
+++ CommandHandler/Middleware.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace CommandHandler;
+
+class Middleware
+{
+
+    private $before = [];
+    private $after = [];
+
+    public function __construct($before, $after)
+    {
+        $this->before = $this->callbackFilter($before);
+        $this->after = $this->callbackFilter($after);
+    }
+
+    public function addBefore()
+    {
+        $this->before = array_merge(
+            $this->before,
+            $this->callbackFilter(func_get_args())
+        );
+    }
+
+    public function addAfter()
+    {
+        $this->after = array_merge(
+            $this->after,
+            $this->callbackFilter(func_get_args())
+        );
+    }
+
+    public function before()
+    {
+        return $this->call($this->before);
+    }
+
+    public function after($value)
+    {
+        return $this->call($this->after, $value);
+    }
+
+    private function call(array $list, $result = null)
+    {
+        foreach ($list as $item) {
+            $result = call_user_func_array($item, [$item]);
+            if ($result === false) {
+                return false;
+            }
+        }
+
+        return $result;
+    }
+
+    private function callbackFilter(array $list)
+    {
+        return array_filter($list, function ($item)
+        {
+            return is_callable($item);
+        });
+    }
+
+}
